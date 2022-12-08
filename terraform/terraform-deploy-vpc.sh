45c93aa8cc6d4f89f1782bb7829e76fe2c62bfb5
unknown <bhavyajainbt@gmail.com>
2022-12-08 15:42:00 +0530

    subnet files updated 2

--- terraform/terraform-deploy-vpc.sh
+++ terraform/terraform-deploy-vpc.sh
@@ -10,10 +10,10 @@ else
   region_name=$5
   cd terraform/
   cd ${env_dir}
-  if [ $1 -eq 'vpc' ];then
+  if [ $1 = "vpc" ];then
     terraform init
     terraform apply -var vpc_cidr_block=${cidr_block} -var vpc_tag_name=${tag_name} -var vpc_region=${region_name} -target=aws_vpc.node-casted-vpc-dev  -auto-approve
-  elif [ $1 -eq "subnets" ];then
+  elif [ $1 = "subnets" ];then
     terraform init
     terraform apply -var subnet_cidr_block=${cidr_block} -var subnet_tag_name=${tag_name} -var vpc_region=${region_name} -target=aws_subnet.node-casted-vpc-subnet  -auto-approve
   else
