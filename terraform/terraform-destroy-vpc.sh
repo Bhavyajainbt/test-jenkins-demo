10c0f5528d1cc4c5e2735ef18124acc2549b4575
unknown <bhavyajainbt@gmail.com>
2022-12-08 14:24:53 +0530

    updated script file destroy code

--- terraform/terraform-destroy-vpc.sh
+++ terraform/terraform-destroy-vpc.sh
@@ -12,4 +12,4 @@ cd ${env_dir}
 echo "Moved to sub environment directory"
 terraform init
 echo "terraform init is successful"
-terraform destroy -auto-approve
+terraform destroy -var vpc_cidr_block=${cidr_block} -var vpc_tag_name=${tag_name} -var vpc_resource_name=${resource_name} -target=aws_vpc.vpc_resource_name  -auto-approve
